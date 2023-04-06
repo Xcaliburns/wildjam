d1c9039d33a3ac098528afa918360e140024fa7c
Yilmaz Kadir <kadir-yilmaz_student2022@wilder.school>
2023-04-06 19:23:05 +0200

    Player faaaaaaaalllllllllll!!!!!

--- Assets/Scripts/WeakSpot.cs
+++ Assets/Scripts/WeakSpot.cs
@@ -1,14 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WeakSpot : MonoBehaviour
 {
+    public GameObject objectToDestroy;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            Destroy(transform.parent.gameObject);
+            Destroy(objectToDestroy);
         }
     }
 }
